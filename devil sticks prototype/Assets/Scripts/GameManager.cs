8c497212c49c4ab55d08e66455aea80f295bcf1d
nBieri10 <59703208+nBieri10@users.noreply.github.com>
2020-01-23 14:19:14 -0500

    UI points added

---
+++ devil sticks prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    int score;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void Addpoint()
+    {
+        score += 1;
+    }
+}
