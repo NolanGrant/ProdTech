75f4aaba2ee7ea9eb230805493e41133dde72ac1
chavaiz <chavaiz@live.com>
2020-01-30 21:59:39 -0500

    the visualizer killed my unity and i couldn't get it to do the thing :anguished:

--- devil sticks prototype/Assets/Scripts/AudioVisualizer.cs
+++ devil sticks prototype/Assets/Scripts/AudioVisualizer.cs
@@ -128,7 +128,32 @@ public class AudioVisualizer : MonoBehaviour
 
     private void UpdateRing()
     {
+        for (int i = 0; i < lineRenderers.Length; i++)
+        {
+            float t = i / (lineRenderers.Length - 2f);
+            float a = t * Mathf.PI * 2f;
 
+            Vector2 direction = new Vector2(Mathf.Cos(a), Mathf.Sin(a));
+            float maximumRadius = (radius + bufferSizeArea + extendLengths[i]);
+
+            lineRenderers[i].SetPosition(0, direction * radius);
+            lineRenderers[i].SetPosition(1, direction * maximumRadius);
+
+            lineRenderers[i].startWidth = Spacing(radius);
+            lineRenderers[i].endWidth = Spacing(maximumRadius);
+
+            lineRenderers[i].startColor = colorGradientA.Evaluate(0);
+            lineRenderers[i].endColor = colorGradientA.Evaluate((extendLengths[i] - 1) / 
+                (maximumExtendLength - 1f));
+
+        }
+    }
+
+    private float Spacing(float radius)
+    {
+        float c = 2f * Mathf.PI * radius;
+        float n = lineRenderers.Length;
+        return c / n;
     }
 
 }
