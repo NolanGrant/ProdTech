0e5ae91c1fb54de004ad44e9dadf393a6a158f6b
nBieri10 <59703208+nBieri10@users.noreply.github.com>
2020-01-31 23:27:13 -0500

    Added bomb particle effect on collision

--- devil sticks prototype/Assets/Scripts/Bomb.cs
+++ devil sticks prototype/Assets/Scripts/Bomb.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    public ParticleSystem bombParticles;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "playerCircle")
         {
             collision.gameObject.GetComponentInParent<BombContact>().DisableCircle();
+            Instantiate(bombParticles, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
 
         if (collision.gameObject.tag == "playerTriangle")
         {
             collision.gameObject.GetComponentInParent<BombContact>().DisableTriangle();
+            Instantiate(bombParticles, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
