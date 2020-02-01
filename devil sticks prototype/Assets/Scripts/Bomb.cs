18f6e359558bb895da624923bbb0c36d165a32bc
nBieri10 <59703208+nBieri10@users.noreply.github.com>
2020-01-31 23:19:08 -0500

    Finished implementing bomb object

--- devil sticks prototype/Assets/Scripts/Bomb.cs
+++ devil sticks prototype/Assets/Scripts/Bomb.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "playerCircle")
+        {
+            collision.gameObject.GetComponentInParent<BombContact>().DisableCircle();
+            Destroy(this.gameObject);
+        }
+
+        if (collision.gameObject.tag == "playerTriangle")
+        {
+            collision.gameObject.GetComponentInParent<BombContact>().DisableTriangle();
+            Destroy(this.gameObject);
+        }
     }
 }
--- devil sticks prototype/Assets/Scripts/BombContact.cs
+++ devil sticks prototype/Assets/Scripts/BombContact.cs
@@ -14,40 +14,48 @@ public class BombContact : MonoBehaviour
     bool triangleDisabled = false;
     float tDisableTime = 4;
     float currentTDisableTime;
+    float currentTFlashTime;
+    float tFlashTime = 0.2f;
+    bool tFlash = false;
+
     bool circleDisabled = false;
     float cDisableTime = 4;
     float currentCDisableTime;
+    float currentCFlashTime;
+    float cFlashTime = 0.2f;
+    bool cFlash = false;
 
     // Start is called before the first frame update
     void Start()
     {
-       circleCollider = circle.GetComponent<CircleCollider2D>();
-       triangleCollider = triangle.GetComponent<PolygonCollider2D>();
+       circleCollider = circle.GetComponentInChildren<CircleCollider2D>();
+       triangleCollider = triangle.GetComponentInChildren<PolygonCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         FlashShapes();
+        ResetShapes();
     }
 
     public void DisableCircle()
     {
-     if (circleDisabled)
+     if (!circleDisabled)
         {
             currentCDisableTime = Time.time;
             circleCollider.enabled = false;
-            circleDisabled = false;
+            circleDisabled = true;
         }
     }
 
     public void DisableTriangle()
     {
-        if (triangleDisabled)
+        if (!triangleDisabled)
         {
             currentTDisableTime = Time.time;
             triangleCollider.enabled = false;
-            triangleDisabled = false;
+            triangleDisabled = true;
         }
     }
 
@@ -55,15 +63,62 @@ public class BombContact : MonoBehaviour
     {
         if (triangleDisabled)
         {
+            if (!tFlash && Time.time > currentTFlashTime + tFlashTime)
+            {
+                tSR.enabled = false;
+                currentTFlashTime = Time.time;
+                tFlash = true;
+            }
 
+            if (tFlash && Time.time > currentTFlashTime + tFlashTime)
+            {
+                tSR.enabled = true;
+                currentTFlashTime = Time.time;
+                tFlash = false;
+            }
         }
+        else
+        {
+            tSR.enabled = true;
+            tFlash = false;
+        }
+
+        if (circleDisabled)
+        {
+            if (!cFlash && Time.time > currentCFlashTime + cFlashTime)
+            {
+                cSR.enabled = false;
+                currentCFlashTime = Time.time;
+                cFlash = true;
+            }
+
+            if (cFlash && Time.time > currentCFlashTime + cFlashTime)
+            {
+                cSR.enabled = true;
+                currentCFlashTime = Time.time;
+                cFlash = false;
+            }
+        }
+        else
+        {
+            cSR.enabled = true;
+            cFlash = false;
+        }
+
     }
 
     void ResetShapes()
     {
         if (triangleDisabled && Time.time > currentTDisableTime + tDisableTime)
         {
+            triangleCollider.enabled = true;
+            triangleDisabled = false;
+        }
 
+        if (circleDisabled && Time.time > currentCDisableTime + cDisableTime)
+        {
+            circleCollider.enabled = true;
+            circleDisabled = false;
         }
     }
 }
