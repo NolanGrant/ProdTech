515d69681f38fbea6760cc18ed6d16af14dacb38
nBieri10 <59703208+nBieri10@users.noreply.github.com>
2020-02-01 12:19:07 -0500

    Added speed script to speed up player rotation

---
+++ devil sticks prototype/Assets/Scripts/Speed.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+    float fastRotate;
+    float setRotate;
+    bool rotateFaster = false;
+    Player playerScript;
+    GameManager gmScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gmScript = FindObjectOfType<GameManager>();
+        playerScript = GetComponent<Player>();
+        setRotate = playerScript.rotationForce;
+        fastRotate = playerScript.rotationForce * 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateFast();
+    }
+
+    void RotateFast()
+    {
+        if (gmScript.increaseSpeed && !rotateFaster)
+        {
+            playerScript.rotationForce = fastRotate;
+            rotateFaster = true;
+        }
+
+        if (!gmScript.increaseSpeed && rotateFaster)
+        {
+            playerScript.rotationForce = setRotate;
+            rotateFaster = false;
+            return;
+        }
+    }
+}
