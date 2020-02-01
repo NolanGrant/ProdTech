602597dad85a094fce88dd0c02922f94e0b5fe02
chavaiz <chavaiz@live.com>
2020-02-01 13:44:25 -0500

    adding miss sound for failing to hit object

--- devil sticks prototype/Assets/Scripts/PlayerAudioHandler.cs
+++ devil sticks prototype/Assets/Scripts/PlayerAudioHandler.cs
@@ -6,18 +6,22 @@ public class PlayerAudioHandler : MonoBehaviour
 {
     AudioSource playerAudio;
     AudioClip impact;
+    AudioClip miss;
+
+    GameObject rotatingObject;
 
     // Start is called before the first frame update
     void Start()
     {
         playerAudio = GetComponent<AudioSource>();
         impact = playerAudio.clip;
+        miss = playerAudio.clip;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotatingObject = GameObject.Find("Rotating Object");
     }
 
     public void PlaySound()
@@ -25,4 +29,13 @@ public class PlayerAudioHandler : MonoBehaviour
         playerAudio.pitch = Random.Range(0.5f, 2f);
         playerAudio.PlayOneShot(impact);
     }
+
+    public void PlayMissSound()
+    {
+        if (rotatingObject.transform.position.x <= -10)
+        {
+            playerAudio.PlayOneShot(miss);
+        }
+    }
+
 }
